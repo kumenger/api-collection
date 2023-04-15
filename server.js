8458b1370dd778bd4482cb0bd926c548055f4b78
kumenger <44558777+kumenger@users.noreply.github.com>
2023-04-14 23:41:54 -0600

    test redeploye

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express=require('express')
-const cros=require('cors')
-const bodyParser=require('body-parser')
-const serchRouter=require('./routes/serachRouts')
-const app=express()
-app.use(cros())
+const express = require("express");
+const cros = require("cors");
+const bodyParser = require("body-parser");
+const serchRouter = require("./routes/serachRouts");
+const app = express();
+app.use(cros());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const path =require('path')
-app.use('/api',serchRouter)
+const path = require("path");
+app.use("/api", serchRouter);
 
-const PORT=process.env.PORT||5000
-app.use(express.static(path.join(__dirname, "client", "build")))
+const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-app.listen(PORT,()=>{
-    console.log(`running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
