fa836a8260a6d937d0da37b9dedeb2174b7fe2a4
kumenger <44558777+kumenger@users.noreply.github.com>
2023-04-18 05:56:24 -0600

    redeploye

--- client/src/components/actions/index.js
+++ client/src/components/actions/index.js
@@ -1,26 +1,6 @@
 import axios from 'axios'
-import { json } from 'body-parser';
-import qs from 'qs'
-/*export  const getSearc=(term)=>async dispatch=>{
-    const response= await axios.get(`
-    https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srsearch=${term}`)
-    dispatch({type:"Get_Result",payload:response.data.query.search})}*/
-   
 
-   /*export const getNews=(newsToBeSearched)=> async dispatch=>{
-        var options = {
-            method: 'GET',
-            url: 'https://bing-news-search1.p.rapidapi.com/news/search',
-            params: {q: newsToBeSearched, freshness: 'Day', textFormat: 'Raw', safeSearch: 'Strict',mkt:'en-US',count:20,originalImg:true},
-            headers: {
-              'x-bingapis-sdk': 'true',
-              'x-rapidapi-key': '6a7d25eaecmshee44f344abc6669p1ffe03jsnf5b51c7e8f40',
-              'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com'
-            }
-          };
-        const respose=await axios.request(options)
-       
-      dispatch({type:"Get_Result",payload:respose.data.value})   }*/
+
      
   
  
